179de3558cdfe6d5d820b726c588719b2c20ee99
Orlando <opseua@gmail.com>
2023-02-24 19:19:56 -0300

    Adicionado 'SHA1' no arquivo do MySQL

--- src/model/banco_de_dados_usuarios.sql
+++ src/model/banco_de_dados_usuarios.sql
@@ -24,14 +24,14 @@ CREATE TABLE usuarios (
 -- Inserir dados na tabela
 INSERT INTO usuarios (usuario_nome, usuario_genero, usuario_nascimento, usuario_peso, usuario_altura, usuario_tipo_sanguineo, usuario_imc, usuario_email, usuario_senha, usuario_reset_pergunta, usuario_reset_resposta, usuario_status) 
 VALUES 
-('Orlando','Masculino','2001-01-01','75.2','1.85','A+','25.41','orlando@gmail.com','senha_12345678@','Comida favorita?','maça', 'on'),
-('Thiago','Masculino','2002-02-02','80.6','1.73','O-','35.61','thiago@gmail.com','senha_12345678@','Cor favorita?','azul', 'on'),
-('Matheus','Masculino','2003-03-03','50.7','1.65','O-','24.72','matheus@gmail.com','senha_12345678@','Filme favorito?','Avatar', 'on'),
-('Gabrielly','Feminino','2004-04-04','90.5','1.75','O-','19.83','gabrielly@gmail.com','senha_12345678@','Prefere calor ou frio?','Frio', 'on'),
-('Priscila','Feminino','2005-05-05','72.3','1.84','AB-','17.94','priscila@gmail.com','senha_12345678@','Nome do pai?','Rogerio', 'on'),
-('Yasmim','Feminino','2006-06-06','83.2','1.77','A+','29.55','yasmim@gmail.com','senha_12345678@','Música favorita?','Anita - tal tal tal ', 'on'),
-('Juliano','Masculino','2007-07-07','59.2','1.61','AB-','30.56','juliano@gmail.com','senha_12345678@','Comida favorita?','Morango', 'off'),
-('Marcos','Masculino','2008-08-08','74.3','1.99','O-','27.47','marcos@gmail.com','senha_12345678@','Cor favorita?','Vermelho', 'off'),
-('Mariana','Feminino','2009-09-09','61.8','1.74','A+','22.28','mariana@gmail.com','senha_12345678@','Comida favorita?','Arroz', 'del'),
-('Juliana','Feminino','2010-10-10','73.4','1.88','AB-','18.89','juliana@gmail.com','senha_12345678@','Filme favorito?','Velozes e furiosos', 'del');
+('Orlando','Masculino','2001-01-01','75.2','1.85','A+','25.41','orlando@gmail.com',SHA1('senha_@_user_1'),'Comida favorita?','maça', 'on'),
+('Thiago','Masculino','2002-02-02','80.6','1.73','O-','35.61','thiago@gmail.com',SHA1('senha_@_user_2'),'Cor favorita?','azul', 'on'),
+('Matheus','Masculino','2003-03-03','50.7','1.65','O-','24.72','matheus@gmail.com',SHA1('senha_@_user_3'),'Filme favorito?','Avatar', 'on'),
+('Gabrielly','Feminino','2004-04-04','90.5','1.75','O-','19.83','gabrielly@gmail.com',SHA1('senha_@_user_4'),'Prefere calor ou frio?','Frio', 'on'),
+('Priscila','Feminino','2005-05-05','72.3','1.84','AB-','17.94','priscila@gmail.com',SHA1('senha_@_user_5'),'Nome do pai?','Rogerio', 'on'),
+('Yasmim','Feminino','2006-06-06','83.2','1.77','A+','29.55','yasmim@gmail.com',SHA1('senha_@_user_6'),'Música favorita?','Anita - tal tal tal ', 'on'),
+('Juliano','Masculino','2007-07-07','59.2','1.61','AB-','30.56','juliano@gmail.com',SHA1('senha_@_user_7'),'Comida favorita?','Morango', 'off'),
+('Marcos','Masculino','2008-08-08','74.3','1.99','O-','27.47','marcos@gmail.com',SHA1('senha_@_user_8'),'Cor favorita?','Vermelho', 'off'),
+('Mariana','Feminino','2009-09-09','61.8','1.74','A+','22.28','mariana@gmail.com',SHA1('senha_@_user_9'),'Comida favorita?','Arroz', 'del'),
+('Juliana','Feminino','2010-10-10','73.4','1.88','AB-','18.89','juliana@gmail.com',SHA1('senha_@_user_10'),'Filme favorito?','Velozes e furiosos', 'del');
 
