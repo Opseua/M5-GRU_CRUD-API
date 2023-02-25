67f16e4057c4245181a4c570ad66b75c5fe79d13
Orlando <opseua@gmail.com>
2023-02-25 20:23:51 -0300

    Heroku setup

--- src/model/mysql.js
+++ src/model/mysql.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2');
 const conf = require('dotenv').config().parsed;
 
 const conn = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password:"",
-  database: "banco_de_dados",
+  host: "eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ccsoc33936pri06k",
+  password: "t7e8zbtnprkhbx5a",
+  database: "a8lzgywbc4gzr889",
   port: "3306"
 }).promise();
 
