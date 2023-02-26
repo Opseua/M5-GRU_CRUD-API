418d951e80da4d20523b258b5c59bc93a7335bea
Orlando <opseua@gmail.com>
2023-02-25 21:53:56 -0300

    Porta alterdada

--- src/model/mysql.js
+++ src/model/mysql.js
@@ -4,12 +4,11 @@ const mysql = require('mysql2');
 const conf = require('dotenv').config().parsed;
 
 const conn = mysql.createPool({
-  host: conf.HOSTNAME,
-  user: conf.USERNAME,
-  password: conf.PASSWORD,
-  database: conf.DATABASE,
-  port: conf.HOSTPORT,
-  inf: conf.TESTE
+  host: "eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ccsoc33936pri06k",
+  password: "t7e8zbtnprkhbx5a",
+  database: "a8lzgywbc4gzr889",
+  port: "3306"
 }).promise();
 
 // Exporta o módulo de conexão com o MySQL.
