ce134a39057aa2d6ab61c473af7b97e1fa50f08c
Orlando <opseua@gmail.com>
2023-02-25 21:10:52 -0300

    .env alterado

--- src/model/mysql.js
+++ src/model/mysql.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2');
 const conf = require('dotenv').config().parsed;
 
 const conn = mysql.createPool({
-  host: "sql.freedb.tech",
-  database: "freedb_M5-GRU_CRUD-API",
-  user: "freedb_Usuario_M5-GRU_CRUD-API",
-  password: "AN*57mHa8qXzaS#",
-  port: "3306"
+  host: conf.HOSTNAME,
+  user: conf.USERNAME,
+  password: conf.PASSWORD,
+  database: conf.DATABASE,
+  port: conf.HOSTPORT
 }).promise();
 
 // Exporta o módulo de conexão com o MySQL.
