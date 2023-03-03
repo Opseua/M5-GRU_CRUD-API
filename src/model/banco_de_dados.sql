d2cba26176615ef2a2ca6cb4bdae1ec767e07d5a
Orlando <opseua@gmail.com>
2023-03-03 17:08:50 -0300

    Adicionado MySQL 'funcionalidades'

--- src/model/banco_de_dados.sql
+++ src/model/banco_de_dados.sql
@@ -101,3 +101,17 @@ VALUES
 
 -- ###########################################################################################
 
+-- Criar tabela (Feito por: ORLANDO)
+CREATE TABLE funcionalidades (
+	funcionalidade_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	funcionalidade_titulo VARCHAR(255) NOT NULL,
+	funcionalidade_descricao VARCHAR(255) NOT NULL
+);
+
+-- Inserir dados na tabela
+INSERT INTO funcionalidades (funcionalidade_titulo, funcionalidade_descricao) 
+VALUES 
+('Cadastrar notas','Com a nossa aplicação você pode cadastrar quantas notas quiser.'),
+('Visualizar','Todas as suas notas ficam registradas e podem ser lidas a qualquer momento!'),
+('Editar','Precisa editar alguma nota? Isso também é possível!'),
+('Remover','Não precisa mais da nota... sem problemas no Caderno de Saúde você pode apagar um nota sempre que precisar.');
